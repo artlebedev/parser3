d7e1265dd09c6598df7dba5a41c6f325911709ea
Alexander Petrossian <paf@design.ru>
2001-02-13 12:38:00 +0000

    operators : and self. prefixes

--- src/main/core.C
+++ src/main/core.C
@@ -1,21 +1,20 @@
-void process(Pool& pool, 
-			 Value& self,
+void process(Method_self_n_params& root, Value& self,
 			 RContext& rcontext, WContext& wcontext, 
 			 String_iterator& code, char char_to_stop_before) {
 	
 	// $ on code?
-	process_dollar(pool, rcontext, wcontext, code, char_to_stop_before);
+	process_dollar(root, self, rcontext, wcontext, code, char_to_stop_before);
 
 	// ^ on code?
-	process_bird(pool, rcontext, wcontext, code, char_to_stop_before);
+	process_bird(root, self, rcontext, wcontext, code, char_to_stop_before);
 
 	// TODO
-	// 1. случаи :: и this. в начале 
+	// 1! случаи : и self. в начале 
 	// 2. вызовы статических методов
+	// 3! вызовы операторов
 }
 
-void process_dollar(Pool& pool, 
-					Value& self,
+void process_dollar(Method_self_n_params& root, Value& self,
 					RContext& rcontext, WContext& wcontext, 
 					String_iterator& iter, char char_to_stop_before) {
 
@@ -25,12 +24,16 @@ void process_dollar(Pool& pool,
 	
 	Array/*<String&>*/ names(pool);  // what.they.refer.to left-to-right list
 	char names_ended_before; // the char after long name
+	Prefix prefix;
 	get_names(
 		iter, " ([",
-		&names, &names_ended_before); // must return count()>0
+		&prefix, &names, &names_ended_before); // can return count()=0 when $self alone
 
 	bool read_mode=name_ended_before==' ';
-	Value *context=read_mode?rcontext:wcontext;
+	Value *context=
+		prefix?
+			prefix==ROOT?root:self:
+		read_mode?rcontext:wcontext;
 	
 	if(read_mode) {
 		// 'context' dive into dotted path
@@ -49,7 +52,8 @@ void process_dollar(Pool& pool,
 		int steps=names.count();
 		// if constructing then "excluding last .name"
 		if(construct_mode)
-			steps--;
+			if(!steps--) // bad: "$self("; now we can safely do ".get[steps]" below
+				pool.exception().raise("self re-construction prohibited");
 		for(int i=0; i<steps; i++) {
 			String& name=static_cast<String&>(names.get[i]);
 			Value *next_current=context->get_element(name);
@@ -64,12 +68,12 @@ void process_dollar(Pool& pool,
 			// last .name
 			String& name=static_cast<String&>(names.get[steps]);
 			WContext local_wcontext(pool /* empty */);
-			process(pool, rcontext, local_wcontext, iter, ')');
+			process(root, self, rcontext, local_wcontext, iter, ')');
 			context->put_element(name, local_wcontext.value());
 		} else { // =='['
 			// .name[with-code], processing on 'context'
 			WContext local_context(pool, context);
-			process(pool, local_context, local_context, iter, ']');
+			process(root, self, local_context, local_context, iter, ']');
 			wcontext.write(local_context);
 		}
 		
@@ -77,8 +81,7 @@ void process_dollar(Pool& pool,
 	}
 }
 
-void process_bird(Pool& pool, 
-				  Value& self,
+void process_bird(Method_self_n_params& root, Value& self,
 				  RContext& rcontext, WContext& wcontext, 
 				  String_iterator& iter, char char_to_stop_before) {
 	
@@ -87,19 +90,24 @@ void process_bird(Pool& pool,
 	
 	Array/*<String&>*/ names(pool);  // what.they.refer.to left-to-right list
 	char names_ended_before; // the char after long name
+	Prefix prefix;
 	get_names(
 		iter, "[",
-		&names, &names_ended_before); // must return count()>0
+		&prefix, &names, &names_ended_before); // can return count()=0 when ^self alone
 
-	Value *context=rcontext;
+	Value *context=
+		prefix?
+			prefix==ROOT?root:self:
+		rcontext;
 	iter++; // skip '['
 
 	// 'context' dive into dotted path, excluding last .name
-	Value *local_self=context;
 	int steps=names.count()-1;
+	if(steps<0) // bad: "^self["; now we can safely do ".get[steps]" below
+		pool.exception().raise("call: calling method named 'self'");
 	for(int i=0; i<steps; i++) {
 		String& name=static_cast<String&>(names.get[i]);
-		context=(local_self=context)->get_element(name);
+		context=context->get_element(name);
 		if(!context) // no such object field, sad story: can't call method of void
 			pool.exception().raise(name, "call: to void.method");
 	}
@@ -110,26 +118,33 @@ void process_bird(Pool& pool,
 	Method *method=context.get_method(name);
 	if(!method) { // no such method: try to locate method ref field
 		Value *value=context.get_element(name);
-		if(!value) // failed: no element of that 'name'
-			pool.exception().raise(name, "call: no method field found");
-		Method_ref *method_ref=value->get_method_ref();
-		if(!method_ref) // failed: that field wasn't method_ref
-			pool.exception().raise(name, "call: this field is not a method reference");
-		method=method_ref->method;
-		local_self=method_ref->self;
+		if(value) { // good: we have some element of that 'name'
+			Method_ref *method_ref=value->get_method_ref();
+			if(!method_ref) // bad: that field wasn't method_ref
+				pool.exception().raise(name, "call: this field is not a method reference");
+			method=method_ref->method;
+			context=method_ref->self;
+		} else { // no element of that 'name', that must be operator then
+			method=operators.get(name);
+			if(!method) // bad: that 'name' is neither method nor field nor operator
+				pool.exception().raise(name, "call: neither method nor field nor operator");
+		}
 	}
 
 
 	Array/*<String&>*/ param_values(pool);
 	get_params(
-		iter, "]",
+		iter,
 		&param_values);
 	iter++; // skip ']'
 
 	Method_self_n_params local_rcontext(pool, 
-		local_self,
+		context,
 		method->param_names, param_values);
-	WContext local_wcontext(pool /* empty */);
-	process(pool, local_rcontext, local_wcontext, iter, ']');
+	WContext local_wcontext(pool, local_self);
+	process(
+		context, local_rcontext, 
+		local_rcontext, local_wcontext, 
+		iter, ']');
 	wcontext.write(local_wcontext);
 }
