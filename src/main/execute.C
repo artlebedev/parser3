3afb297a1e384de19b62444fade8f0ee7a29b3c6
Alexander Petrossian <paf@design.ru>
2001-02-21 07:40:52 +0000

    z

--- src/main/execute.C
+++ src/main/execute.C
@@ -1,8 +1,8 @@
 /*
-  $Id: execute.C,v 1.5 2001/02/21 07:40:21 paf Exp $
+  $Id: execute.C,v 1.6 2001/02/21 07:40:52 paf Exp $
 */
 
-#include "pa_array.h"
+#include "pa_array.h" 
 #include "code.h"
 
 #include <stdio.h>
