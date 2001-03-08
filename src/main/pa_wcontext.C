0d9102b8a1fd52d090f3e7508441e6c247702567
Alexander Petrossian <paf@design.ru>
2001-03-08 15:50:53 +0000

    z

--- src/main/pa_wcontext.C
+++ src/main/pa_wcontext.C
@@ -1,5 +1,5 @@
 /*
-  $Id: pa_wcontext.C,v 1.5 2001/03/08 13:13:41 paf Exp $
+  $Id: pa_wcontext.C,v 1.6 2001/03/08 15:50:53 paf Exp $
 */
 
 #include "pa_wcontext.h"
@@ -19,8 +19,9 @@ void WContext::write(Value& avalue) {
 	else
 		if(fvalue) // already have value?
 			THROW(0,0,  // must not construct twice
-			&fvalue->name(),
-			"value already assigned, use constructor to reassign it");
+			&avalue.name(),
+			"(%s) illegal assignment attempt to '%s' (%s), use constructor instead",
+				avalue.type(), fvalue->name().cstr(), fvalue->type());
 		else
 			fvalue=&avalue;
 }
