e151f97e4d9410bcc180984aa6a009685f2ffef4
Alexander Petrossian <paf@design.ru>
2001-04-03 08:38:57 +0000

    z

---
+++ src/types/pa_vstring.C
@@ -0,0 +1,19 @@
+/** @file
+	Parser: string class.
+
+	Copyright(c) 2001 ArtLebedev Group(http://www.artlebedev.com)
+
+	Author: Alexander Petrosyan <paf@design.ru>(http://design.ru/paf)
+
+	$Id: pa_vstring.C,v 1.1 2001/04/03 08:38:57 paf Exp $
+*/
+
+#include "pa_vstring.h"
+#include "pa_vfile.h"
+
+const VFile *VString::as_vfile() const {
+	VFile& result=*NEW VFile(pool());
+	const char *cstr=fstring.cstr();
+	result.set(cstr, strlen(cstr));
+	return &result;
+}
