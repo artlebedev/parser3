3f3ffe2344b1a1ffb2726d0ca869234392cf8d63
parser <parser>
2001-05-28 09:03:25 +0000

    removed >a

--- src/doc/html2chm.cmd
+++ src/doc/html2chm.cmd
@@ -1,6 +1,6 @@
 CD html
 SET dest=..\chm
-START /low /wait "" "D:\Program Files\HTML Help Workshop\hhc.exe" index.hhp > a
+START /low /wait "" "D:\Program Files\HTML Help Workshop\hhc.exe" index.hhp
 ATTRIB +h index.hh?
 DEL %dest%\*.ch? 
 COPY *.ch? %dest%\parser3.*
