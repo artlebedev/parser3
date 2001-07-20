1317a65d4f80dafffeb036f551a6dab5563d5d67
parser <parser>
2001-07-20 09:43:22 +0000

    X!$browser:type

--- src/types/pa_vrequest.C
+++ src/types/pa_vrequest.C
@@ -5,7 +5,7 @@
 
 	Author: Alexander Petrosyan <paf@design.ru>(http://design.ru/paf)
 
-	$Id: pa_vrequest.C,v 1.7 2001/07/20 09:40:46 parser Exp $
+	$Id: pa_vrequest.C,v 1.8 2001/07/20 09:43:22 parser Exp $
 */
 
 #include "pa_vrequest.h"
@@ -13,40 +13,6 @@
 
 // request: CLASS,method,field
 Value *VRequest::get_element(const String& aname) {
-	// $browser
-	if(aname=="browser") {
-		VHash *vhash=NEW VHash(pool());
-
-		// analize
-		bool ie=true;
-		const char *version=0;
-		const char *agent=frequest.info.user_agent;
-		if(agent) {
-			if(strstr(agent, "compatible")) {
-				if(const char *msie=strstr(agent, "MSIE ")) {
-					ie=true;
-					version=msie+5;
-				}
-			} else {
-				if(!strncasecmp(agent, "MOZILLA/", 8)) {
-					ie=false;
-					version=agent+8;
-				}
-			}
-		}
-		
-		// set $browser.type
-		vhash->hash().put(*NEW String(pool(), "type"), 
-			NEW VString(*NEW String(pool(), ie?"ie":"nn")));
-		if(version) { // we know the version
-			// set $browser.version
-			vhash->hash().put(*NEW String(pool(), "version"), 
-				NEW VDouble(pool(), atof(version)));
-		}
-		//
-		return vhash;
-	}
-
 	// $query $uri 
 	const char *cstr=0;
 	if(aname=="query")
