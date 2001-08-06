f10b3737b44ec714d0e3588e97de56b8eb6dfc11
parser <parser>
2001-08-06 09:33:19 +0000

    hash:keys renamed to hash:_keys :(

--- src/classes/hash.C
+++ src/classes/hash.C
@@ -5,9 +5,9 @@
 
 	Author: Alexander Petrosyan <paf@design.ru> (http://design.ru/paf)
 
-	$Id: hash.C,v 1.12 2001/07/23 12:41:47 parser Exp $
+	$Id: hash.C,v 1.13 2001/08/06 09:33:19 parser Exp $
 */
-static const char *RCSId="$Id: hash.C,v 1.12 2001/07/23 12:41:47 parser Exp $"; 
+static const char *RCSId="$Id: hash.C,v 1.13 2001/08/06 09:33:19 parser Exp $"; 
 
 #include "classes.h"
 #include "pa_request.h"
@@ -184,7 +184,7 @@ MHash::MHash(Pool& apool) : Methoded(apool) {
 	add_native_method("sql", Method::CT_DYNAMIC, _sql, 1, 3);
 
 	// ^hash.keys[]
-	add_native_method("keys", Method::CT_DYNAMIC, _keys, 0, 0);	
+	add_native_method("_keys", Method::CT_DYNAMIC, _keys, 0, 0);	
 }
 
 // global variable
