50660d26b3040c79a0142c999f39006bb1c7945d
parser <parser>
2001-09-15 13:42:17 +0000

    critical section guardian

--- src/targets/apache13/modules/extra/pa_pool.C
+++ src/targets/apache13/modules/extra/pa_pool.C
@@ -4,7 +4,7 @@
 	Copyright (c) 2001 ArtLebedev Group (http://www.artlebedev.com)
 	Author: Alexander Petrosyan <paf@design.ru> (http://design.ru/paf)
 
-	$Id: pa_pool.C,v 1.8 2001/09/15 13:20:22 parser Exp $
+	$Id: pa_pool.C,v 1.9 2001/09/15 13:42:17 parser Exp $
 */
 
 //#include <stdlib.h>
@@ -21,8 +21,19 @@ void *Pool::real_calloc(size_t size) {
 	return ap_pcalloc((pool*)fstorage, size);
 }
 
-void Pool::real_register_cleanup(void (*cleanup) (void *), void *data) {
+bool Pool::real_register_cleanup(void (*cleanup) (void *), void *data) {
+	/* NB any code which invokes register_cleanup ... directly
+	 * is a critical section which should be guarded by block_alarms() and
+	 * unblock_alarms() ...
+	 */
+
+	ap_block_alarms();
+
 	ap_register_cleanup((pool*)fstorage, data,
 		cleanup,
 		ap_null_cleanup);
+
+	ap_unblock_alarms();
+
+	return true;
 }
