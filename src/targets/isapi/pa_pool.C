3f5aaf1f643efdd81c1c2fca9773d8ea49bedd90
Alexander Petrossian <paf@design.ru>
2001-10-29 17:02:22 +0000

    added isapi&apache real_malloc debug param.someday either ifdef or remove it

--- src/targets/isapi/pa_pool.C
+++ src/targets/isapi/pa_pool.C
@@ -4,7 +4,7 @@
 	Copyright (c) 2001 ArtLebedev Group (http://www.artlebedev.com)
 	Author: Alexander Petrosyan <paf@design.ru> (http://design.ru/paf)
 
-	$Id: pa_pool.C,v 1.7 2001/09/26 10:32:26 parser Exp $
+	$Id: pa_pool.C,v 1.8 2001/10/29 17:02:22 paf Exp $
 */
 
 #include <stdlib.h>
@@ -12,7 +12,7 @@
 #include "pa_pool.h"
 #include "pool_storage.h"
 
-void *Pool::real_malloc(size_t size) {
+void *Pool::real_malloc(size_t size, int place) {
 	return fstorage?
 		static_cast<Pool_storage *>(fstorage)->malloc(size): ::malloc(size);
 }
