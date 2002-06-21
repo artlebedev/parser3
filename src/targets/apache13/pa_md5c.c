10d8ad39ba968d72856f662ec08a63c62dc3cb94
Alexander Petrossian <paf@design.ru>
2002-06-21 13:14:34 +0000

    moved libltdl and pcre to libs/ added libs/md5 which is linked to non-apache targets ^math:crypt[password;$apr1$salt]

---
+++ src/targets/apache13/pa_md5c.c
@@ -0,0 +1,23 @@
+/** @file
+	Parser: stub to pass MD5 func calls to apache 1.3 core.
+
+	Copyright (c) 2001, 2002 ArtLebedev Group (http://www.artlebedev.com)
+	Author: Alexandr Petrosian <paf@design.ru> (http://paf.design.ru)
+
+	$Id: pa_md5c.c,v 1.1 2002/06/21 13:14:34 paf Exp $
+*/
+
+#include "pa_md5.h"
+
+#include "ap_config.h"
+#include "ap_md5.h"
+
+void pa_MD5Init(PA_MD5_CTX *context) { ap_MD5Init((AP_MD5_CTX*)context); }
+void pa_MD5Update(PA_MD5_CTX *context, const unsigned char *input,
+			      unsigned int inputLen) { ap_MD5Update((AP_MD5_CTX *)context, input, inputLen); }
+void pa_MD5Final(unsigned char digest[MD5_DIGESTSIZE],
+			     PA_MD5_CTX *context) { ap_MD5Final(digest, (AP_MD5_CTX *)context); }
+void pa_MD5Encode(const unsigned char *password,
+			      const unsigned char *salt,
+			      char *result, size_t nbytes) { ap_MD5Encode(password, salt, result, nbytes); }
+void pa_to64(char *s, unsigned long v, int n) { ap_to64(s, v, n); }
