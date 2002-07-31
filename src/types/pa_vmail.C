5563978fdc0f827d869e8f24656c8c91944e364c
Alexander Petrossian <paf@design.ru>
2002-07-31 15:00:41 +0000

    mail: errors-to: now default "postmaster"

--- src/types/pa_vmail.C
+++ src/types/pa_vmail.C
@@ -6,7 +6,7 @@
 	Copyright(c) 2001, 2002 ArtLebedev Group(http://www.artlebedev.com)
 	Author: Alexandr Petrosian <paf@design.ru>(http://paf.design.ru)
 	
-	$Id: pa_vmail.C,v 1.12 2002/07/31 15:02:17 paf Exp $
+	$Id: pa_vmail.C,v 1.11 2002/07/31 15:00:41 paf Exp $
 */
 
 #include "pa_sapi.h"
@@ -396,7 +396,7 @@ static void store_message_element(const Hash::Key& raw_element_name, Hash::Val *
 		*i.from=&extractEmail(element_value.as_string());
 	if(i.to && low_element_name=="to")
 		*i.to=&extractEmail(element_value.as_string());
-	if(low_element_name=="errors-to")
+	if(i.errors_to && low_element_name=="errors-to")
 		i.errors_to=&extractEmail(element_value.as_string());	
 
 	// append header line
