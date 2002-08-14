33a9b26efe9e10f7b1de49880b17775bff645909
Alexander Petrossian <paf@design.ru>
2002-08-14 12:14:35 +0000

    fixopt now preprocesses command line params, splitting them by space, excluding argv[0], argv[argc-1]

--- src/targets/cgi/fixopt.C
+++ src/targets/cgi/fixopt.C
@@ -5,7 +5,7 @@
 	Author: Alexandr Petrosian <paf@design.ru> (http://paf.design.ru)
 */
 
-static const char* IDENT_FIXOPT_C="$Date: 2002/08/14 11:38:07 $";
+static const char* IDENT_FIXOPT_C="$Date: 2002/08/14 12:14:35 $";
 
 #include "pa_config_includes.h"
 
@@ -20,23 +20,27 @@ void fixopt(
 	int beforec, char **beforev, 
 	int& afterc, char **& aafterv) {
 
+	static char aftervbuf[MAX_ARGV][MAX_STRING];
 	static char *afterv[MAX_ARGV];
 	aafterv=afterv;
 	afterc=0;
 
-	if(beforec)
+	if(beforec) {
 		afterv[afterc++]=beforev[0]; // do not fix argv[0]
 
-	for(int i=1; i<beforec-1; i++) {
-		char *many=beforev[i];
-		while(char *one=lsplit(&many, ' ')) {
-			if(afterc==MAX_ARGV-1 /*so not to check later*/)
-				throw Exception(0,
-					0,
-					"too many command line arguments (more then %d)", MAX_ARGV-1);
-			afterv[afterc++]=one;				
-		}
-	}
+		for(int i=1; i<beforec-1; i++) {
+			char *buf=aftervbuf[afterc];
+			strncpy(buf, beforev[i], MAX_STRING-1); buf[MAX_STRING-1]=0;
 
-	afterv[afterc++]=beforev[beforec-1];
+			while(char *one=lsplit(&buf, ' ')) {
+				if(afterc==MAX_ARGV-1 /*so not to check later*/)
+					throw Exception(0,
+						0,
+						"too many command line arguments (more then %d)", MAX_ARGV-1);
+				afterv[afterc++]=one;				
+			}
+		}
+
+		afterv[afterc++]=beforev[beforec-1];
+	}
 }
