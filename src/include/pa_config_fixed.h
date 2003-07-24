4a9b865784aca11ff8a505657d931fdf2956c83c
Alexander Petrossian <paf@design.ru>
2003-07-24 15:10:42 +0000

    bad #endif fix

--- src/include/pa_config_fixed.h
+++ src/include/pa_config_fixed.h
@@ -10,7 +10,7 @@
 #ifndef PA_CONFIG_FIXED_H
 #define PA_CONFIG_FIXED_H
 
-static const char* IDENT_CONFIG_FIXED_H="$Date: 2003/07/24 14:10:05 $";
+static const char* IDENT_CONFIG_FIXED_H="$Date: 2003/07/24 15:10:42 $";
 
 #define inline  __inline
 #define HAVE_ASSERT_H
