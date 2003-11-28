26e41fe46e9ec0edbea23d6aebe42419663dcd84
Alexander Petrossian <paf@design.ru>
2003-11-28 10:34:39 +0000

    http://localhost/ now checked for safe mode

--- src/main/pa_xml_io.C
+++ src/main/pa_xml_io.C
@@ -9,12 +9,32 @@
 
 #ifdef XML
 
-static const char * const IDENT="$Date: 2003/11/28 09:36:26 $";
+static const char * const IDENT="$Date: 2003/11/28 10:34:39 $";
 
 #include "libxslt/extensions.h"
 
 #include "pa_globals.h"
 #include "pa_request.h"
+
+#ifndef DOXYGEN
+struct MemoryStream {
+	const char* m_buf;
+	size_t m_size;
+	size_t m_position;
+
+	int read(char* a_buffer, size_t a_size) {
+		size_t left=m_size-m_position;
+		if(!left)
+			return 0;
+
+		size_t to_read=min(a_size, left);
+		memcpy(a_buffer, m_buf, to_read);
+		m_position+=to_read;
+		return to_read;
+	}
+
+};
+#endif
 
 /**
  * xmlFileMatchWithLocalhostEqDocumentRoot:
@@ -45,8 +65,9 @@ xmlFileMatchLocalhost(const char* filename) {
  */
 static void *
 xmlFileOpenLocalhost (const char* filename) {
+	Request& r=pa_thread_request();
 	FILE *fd;
-	const char* document_root=pa_thread_request().request_info.document_root;
+	const char* document_root=r.request_info.document_root;
 	if(!document_root)
 		document_root=".";
 
@@ -54,62 +75,13 @@ xmlFileOpenLocalhost (const char* filename) {
 	path[0]=0;
 	strcat(path, document_root);
 	strcat(path, &filename[16]);
-	
-#ifdef WIN32
-	fd = fopen(path, "rb");
-#else
-	fd = fopen(path, "r");
-#endif /* WIN32 */
-	return((void *) fd);
+
+	MemoryStream *stream=new(UseGC) MemoryStream;
+	stream->m_buf=file_read_text(r.charsets, *new String(filename), true);
+	stream->m_size=strlen(stream->m_buf);
+
+	return (void *)stream;
 }
-
-/**
- * xmlFileRead:
- * @context:  the I/O context
- * @buffer:  where to drop data
- * @len:  number of bytes to write
- *
- * Read @len bytes to @buffer from the I/O channel.
- *
- * Returns the number of bytes written
- */
-static int
-pa_xmlFileRead (void * context, char * buffer, int len) {
-	return(fread(&buffer[0], 1,  len, (FILE *) context));
-}
-
-/**
- * xmlFileClose:
- * @context:  the I/O context
- *
- * Close an I/O channel
- */
-static int
-pa_xmlFileClose (void * context) {
-	return ( ( fclose((FILE *) context) == EOF ) ? -1 : 0 );
-}
-
-//////////////////////////
-
-#ifndef DOXYGEN
-struct MemoryStream {
-	const char* m_buf;
-	size_t m_size;
-	size_t m_position;
-
-	int read(char* a_buffer, size_t a_size) {
-		size_t left=m_size-m_position;
-		if(!left)
-			return 0;
-
-		size_t to_read=min(a_size, left);
-		memcpy(a_buffer, m_buf, to_read);
-		m_position+=to_read;
-		return to_read;
-	}
-
-};
-#endif
 
 static int
 xmlFileMatchMethod(const char* filename) {
@@ -169,7 +141,7 @@ void pa_xml_io_init() {
 	// http://localhost/abc -> $ENV{DOCUMENT_ROOT}/abc | ./abc
 	xmlRegisterInputCallbacks(
 		xmlFileMatchLocalhost, xmlFileOpenLocalhost,
-		pa_xmlFileRead, pa_xmlFileClose);
+		pa_xmlFileReadMethod, pa_xmlFileCloseMethod);
 
 	// parser://method/param/here -> ^MAIN:method[/params/here]
 	xmlRegisterInputCallbacks(
