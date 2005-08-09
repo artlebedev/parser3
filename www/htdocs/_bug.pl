1ab887e1767e34a08de3db8f88ee6c5ed43831d8
Alexander Petrossian <paf@design.ru>
2005-08-09 08:19:04 +0000

    . file::exec $charset transcodes env fine

--- www/htdocs/_bug.pl
+++ www/htdocs/_bug.pl
@@ -1,9 +1,12 @@
-#!/usr/bin/perl
+#!c:/perl/bin/perl
 
 #a
-print "header:value from $0\n";
+print "header:value from $ARGV[0]\n";
 print "Header1: lala1111\n";
 print "\n";
 
 print $ENV{QUERY_STRING};
+open BUG, '>_bug.txt';
+print BUG $ENV{QUERY_STRING};
+print BUG $ARGV[0];
 print "done";
