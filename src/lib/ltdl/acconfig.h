f8a978fb5a046728c45218ec4cc10cce936da4bc
Alexander Petrossian <paf@design.ru>
2005-12-29 13:06:58 +0000

    . merged from 3.2.1

---
+++ src/lib/ltdl/acconfig.h
@@ -0,0 +1,8 @@
+/* Define if you have the GNU dld library.  */
+#undef HAVE_DLD
+
+/* Define if you have the libdl library or equivalent.  */
+#undef HAVE_LIBDL
+
+/* Define if you have the shl_load function.  */
+#undef HAVE_SHL_LOAD
