36a784c81acd4860147faa6066ebfd0e63746bdc
Michael Petrushin <misha@design.ru>
2007-02-05 09:12:02 +0000

    - 2007 in help ;)

--- src/targets/cgi/parser3.C
+++ src/targets/cgi/parser3.C
@@ -5,7 +5,7 @@
 	Author: Alexandr Petrosian <paf@design.ru> (http://paf.design.ru)
 */
 
-static const char * const IDENT_PARSER3_C="$Date: 2006/04/09 13:38:47 $";
+static const char * const IDENT_PARSER3_C="$Date: 2007/02/05 09:12:02 $";
 
 #include "pa_config_includes.h"
 
@@ -588,7 +588,7 @@ static void call_real_parser_handler__supress_system_exception(
 static void usage(const char* program) {
 	printf(
 		"Parser/%s\n"
-		"Copyright(c) 2001-2005 ArtLebedev Group (http://www.artlebedev.com)\n"
+		"Copyright(c) 2001-2007 ArtLebedev Group (http://www.artlebedev.com)\n"
 		"Author: Alexandr Petrosian <paf@design.ru> (http://paf.design.ru)\n"
 		"\n"
 		"Usage: %s [options] file\n"
