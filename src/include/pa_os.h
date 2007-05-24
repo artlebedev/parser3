40bb848d01cab4a0660369682719f43f1186bd6f
Michael Petrushin <misha@design.ru>
2007-05-24 10:25:56 +0000

    - consts for blocking locks + some comments were added

--- src/include/pa_os.h
+++ src/include/pa_os.h
@@ -8,7 +8,14 @@
 #ifndef PA_OS_H
 #define PA_OS_H
 
-static const char * const IDENT_OS_H="$Date: 2005/08/09 08:14:50 $";
+static const char * const IDENT_OS_H="$Date: 2007/05/24 10:25:56 $";
+
+#define PA_LOCK_ATTEMPTS 10
+#define PA_LOCK_WAIT_TIMEOUT 1
+
+// 'blocking' mean we will wait till other process release lock 
+// but we'll make PA_LOCK_ATTEMPTS attempts with PA_LOCK_WAIT_TIMEOUT secs delaus between attempts 
+// 'nonblocking' mean we will make only 1 attempt without waiting
 
 int pa_lock_shared_blocking(int fd);
 int pa_lock_exclusive_blocking(int fd);
