e08797d13aa51797485cd1bdee7ab40593cbc324
Michael Petrushin <misha@design.ru>
2007-05-24 10:27:18 +0000

    - locks engines were rewrited: now we don't use system locks which wait till other threads release it but try get lock, if fail wait 1 sec and make 10 attempts.

--- src/main/pa_os.C
+++ src/main/pa_os.C
@@ -5,7 +5,7 @@
 	Author: Alexandr Petrosian <paf@design.ru> (http://paf.design.ru)
 */
 
-static const char * const IDENT_COMMON_C="$Date: 2005/08/09 08:14:52 $"; 
+static const char * const IDENT_COMMON_C="$Date: 2007/05/24 10:27:18 $"; 
 
 #include "pa_config_includes.h"
 #include "pa_os.h"
@@ -16,37 +16,34 @@ static const char * const IDENT_COMMON_C="$Date: 2005/08/09 08:14:52 $";
 
 #ifdef HAVE_FLOCK
 
-int pa_lock_shared_blocking(int fd) { return flock(fd, LOCK_SH); }
-int pa_lock_exclusive_blocking(int fd) { return flock(fd, LOCK_EX); }
-int pa_lock_exclusive_nonblocking(int fd) { return flock(fd, LOCK_EX || LOCK_NB); }
-int pa_unlock(int fd) { return flock(fd, LOCK_UN); }
+#define PA_SH_LOCK LOCK_SH|LOCK_NB
+#define PA_EX_LOCK LOCK_EX|LOCK_NB
+#define PA_ULOCK LOCK_UN
+#define FLOCK(operation) int status=flock(fd, operation);
 
 #else
 #ifdef HAVE__LOCKING
 
-#define FLOCK(operation) lseek(fd, 0, SEEK_SET);  return _locking(fd, operation, 1)
-int pa_lock_shared_blocking(int fd) { FLOCK(_LK_LOCK); }
-int pa_lock_exclusive_blocking(int fd) { FLOCK(_LK_LOCK); }
-int pa_lock_exclusive_nonblocking(int fd) { FLOCK(_LK_NBLCK); }
-int pa_unlock(int fd) { FLOCK(_LK_UNLCK); }
+#define PA_SH_LOCK _LK_NBLCK
+#define PA_EX_LOCK _LK_NBLCK
+#define PA_ULOCK _LK_UNLCK
+#define FLOCK(operation) lseek(fd, 0, SEEK_SET); int status=_locking(fd, operation, 1);
 
 #else
 #ifdef HAVE_FCNTL
 
-#define FLOCK(cmd, arg) struct flock ls={arg, SEEK_SET};  return fcntl(fd, cmd, &ls)
-int pa_lock_shared_blocking(int fd) { FLOCK(F_SETLKW, F_RDLCK); }
-int pa_lock_exclusive_blocking(int fd) { FLOCK(F_SETLKW, F_WRLCK); }
-int pa_lock_exclusive_nonblocking(int fd) { FLOCK(F_SETLK, F_RDLCK); }
-int pa_unlock(int fd) { FLOCK(F_SETLK, F_UNLCK); }
+#define PA_SH_LOCK F_RDLCK
+#define PA_EX_LOCK F_WRLCK
+#define PA_ULOCK F_UNLCK
+#define FLOCK(operation) struct flock ls={operation, SEEK_SET}; int status=fcntl(fd, F_SETLK, &ls); 
 
 #else
 #ifdef HAVE_LOCKF
 
-#define FLOCK(fd, operation) lseek(fd, 0, SEEK_SET);  return lockf(fd, operation, 1)
-int pa_lock_shared_blocking(int fd) { FLOCK(F_LOCK); } // on intel solaris man doesn't have doc on shared blocking
-int pa_lock_exclusive_blocking(int fd) { FLOCK(F_LOCK); }
-int pa_lock_exclusive_nonblocking(int fd) { FLOCK(F_TLOCK); }
-int pa_unlock(int fd) { FLOCK(F_TLOCK); }
+#define PA_SH_LOCK F_TLOCK
+#define PA_EX_LOCK F_TLOCK
+#define PA_ULOCK F_ULOCK
+#define FLOCK(operation) lseek(fd, 0, SEEK_SET); int status=lockf(fd, operation, 1);
 
 #else
 
@@ -56,6 +53,34 @@ int pa_unlock(int fd) { FLOCK(F_TLOCK); }
 #endif
 #endif
 #endif
+
+int pa_lock(int fd, int attempts, int operation){
+	while(true){
+		FLOCK(operation);
+		attempts--;
+		if(status==0 || attempts<=0){
+			return status;
+		}
+		pa_sleep(PA_LOCK_WAIT_TIMEOUT, 0);
+	}
+};
+
+int pa_lock_shared_blocking(int fd) {
+	return pa_lock(fd, PA_LOCK_ATTEMPTS, PA_SH_LOCK);
+}
+
+int pa_lock_exclusive_blocking(int fd) {
+	return pa_lock(fd, PA_LOCK_ATTEMPTS, PA_EX_LOCK);
+}
+
+int pa_lock_exclusive_nonblocking(int fd) {
+	return pa_lock(fd, 1, PA_EX_LOCK);
+}
+
+int pa_unlock(int fd) {
+	return pa_lock(fd, 1, PA_ULOCK);
+}
+
 
 int pa_sleep(unsigned long secs, unsigned long usecs) {
 	for (;  usecs >= 1000000; ++secs, usecs -= 1000000); 
