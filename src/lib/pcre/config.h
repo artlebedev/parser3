2abca7eeeaf3654a88efeafc94484506b859f915
Michael Petrushin <misha@design.ru>
2008-08-15 15:31:54 +0000

    - configuration for PCRE library

---
+++ src/lib/pcre/config.h
@@ -0,0 +1,31 @@
+#ifndef PA_PCRE_CONFIG_H
+#define PA_PCRE_CONFIG_H
+
+#define HAVE_WINDOWS_H 1
+
+#define HAVE_MEMMOVE 1
+#define HAVE_STRERROR 1
+#define HAVE__STRTOI64 1
+
+//#define PCRE_STATIC 1
+
+#define SUPPORT_UTF8 1
+#define SUPPORT_UCP 1
+/* #undef EBCDIC */
+/* #undef BSR_ANYCRLF */
+/* #undef NO_RECURSE */
+
+#define HAVE_LONG_LONG 1
+#define HAVE_UNSIGNED_LONG_LONG 1
+
+#define NEWLINE			10
+#define POSIX_MALLOC_THRESHOLD	10
+#define LINK_SIZE		2
+#define MATCH_LIMIT		10000000
+#define MATCH_LIMIT_RECURSION	MATCH_LIMIT
+
+#define MAX_NAME_SIZE	32
+#undef MAX_NAME_COUNT
+#define MAX_NAME_COUNT	10000
+
+#endif
