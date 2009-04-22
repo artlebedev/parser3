5a4e58925c330a1624c0b305886b7fdbb858a138
Michael Petrushin <misha@design.ru>
2009-04-22 04:34:14 +0000

    - class vregex was added

---
+++ src/types/pa_vregex.C
@@ -0,0 +1,154 @@
+/** @file
+	Parser: @b regex class.
+
+	Copyright(c) 2001-2009 ArtLebedev Group (http://www.artlebedev.com)
+	Author: Alexandr Petrosian <paf@design.ru> (http://paf.design.ru)
+*/
+
+static const char * const IDENT_VREGEX_C="$Date: 2009/04/22 04:34:14 $";
+
+#include "pa_vregex.h"
+#include "pa_vint.h"
+
+
+char* get_pcre_exec_error_text(int exec_result){
+	switch(exec_result){
+		case PCRE_ERROR_BADUTF8:
+		case PCRE_ERROR_BADUTF8_OFFSET:
+			return "UTF-8 validation failed during pcre_exec (%d).";
+			break;
+		default:
+			return "execution error (%d)";
+	}
+}
+
+
+Value& VRegex::as_expr_result(bool/*return_string_as_is=false*/) {
+	return *new VInt(as_int());
+}
+
+void VRegex::regex_options(const String* options, int* result){
+	struct Regex_option {
+		const char* key;
+		const char* keyAlt;
+		int clear;
+		int set;
+		int *result;
+	} regex_option[]={
+		{"i", "I", 0, PCRE_CASELESS, result}, // a=A
+		{"s", "S", 0, PCRE_DOTALL, result}, // ^\n\n$ [default]
+		{"m", "M", PCRE_DOTALL, PCRE_MULTILINE, result}, // ^aaa\n$^bbb\n$
+		{"x", 0, 0, PCRE_EXTENDED, result}, // whitespace in regex ignored
+		{"U", 0, 0, PCRE_UNGREEDY, result}, // ungreedy patterns (greedy by default)
+		{"g", "G", 0, MF_GLOBAL_SEARCH, result+1}, // many rows
+		{"'", 0, 0, MF_NEED_PRE_POST_MATCH, result+1},
+		{"n", 0, 0, MF_JUST_COUNT_MATCHES, result+1},
+		{0, 0, 0, 0, 0}
+	};
+	result[0]=PCRE_EXTRA /* backslash+non-special char causes error */
+			| PCRE_DOTALL /* dot matches all chars including newline char */
+			| PCRE_DOLLAR_ENDONLY /* dollar matches only end of string, but not newline chars */;
+	result[1]=0;
+
+	if(options && !options->is_empty()) 
+		for(Regex_option *o=regex_option; o->key; o++) 
+			if(
+				options->pos(o->key)!=STRING_NOT_FOUND
+				|| (o->keyAlt && options->pos(o->keyAlt)!=STRING_NOT_FOUND)
+			){
+				*o->result &= ~o->clear;
+				*o->result |= o->set;
+			}
+}
+
+
+void VRegex::set(Charset& acharset, const String* aregex, const String* aoptions){
+	if(aregex->is_empty())
+		throw Exception(PARSER_RUNTIME,
+			0,
+			"regexp is empty");
+
+	fcharset=&acharset;
+	fpattern=aregex->cstr(String::L_UNSPECIFIED); // fix any tainted with L_REGEX
+	fpattern_len=strlen(fpattern);
+
+	regex_options(aoptions, foptions);
+}
+
+
+void VRegex::compile(){
+	const char* err_ptr;
+	int err_offset;
+	int options=foptions[0];
+
+	// @todo (for UTF-8): check string & pattern and use PCRE_NO_UTF8_CHECK option 
+	if(fcharset->isUTF8())
+		options|=PCRE_UTF8;
+
+	fcode=pcre_compile(fpattern, options,
+		&err_ptr, &err_offset,
+		fcharset->pcre_tables);
+
+	if(!fcode){
+		throw Exception(PCRE_EXCEPTION_TYPE,
+			new String(fpattern+err_offset, fpattern_len-err_offset, true/*tainted*/),
+			"regular expression syntax error - %s", err_ptr);
+	}
+
+}
+
+
+size_t VRegex::full_info(int type){
+	size_t result;
+	int fullinfo_result=pcre_fullinfo(fcode, fextra, type, &result);
+	if(fullinfo_result<0){
+		throw Exception(PCRE_EXCEPTION_TYPE,
+			new String(fpattern, strlen(fpattern), true),
+			"pcre_full_info error (%d)", fullinfo_result);
+	}
+
+	return result;
+};
+
+
+size_t VRegex::get_info_size(){
+	return full_info(PCRE_INFO_SIZE);
+}
+
+
+size_t VRegex::get_study_size(){
+	return full_info(PCRE_INFO_STUDYSIZE);
+}
+
+void VRegex::study(){
+	if(fstudied)
+		return;
+
+	const char* err_ptr;
+	fextra=pcre_study(fcode, 0/*options*/, &err_ptr);
+
+	if(err_ptr){
+		throw Exception(PCRE_EXCEPTION_TYPE,
+			new String(fpattern, fpattern_len, true),
+			"pcre_study error: %s", err_ptr);
+	}
+
+	fstudied=true;
+}
+
+
+int VRegex::exec(const char* string, size_t string_len, int* ovector, int ovector_size, int prestart){
+	int result=pcre_exec(fcode, fextra, 
+		string, string_len, prestart,
+		0, ovector, ovector_size);
+			
+	if(result<0 && result!=PCRE_ERROR_NOMATCH){
+		throw Exception(PCRE_EXCEPTION_TYPE, 
+			new String(fpattern, fpattern_len, true),
+			get_pcre_exec_error_text(result), result);
+	}
+
+	return result;
+}
+
+
---
+++ src/types/pa_vregex.h
@@ -0,0 +1,136 @@
+/** @file
+	Parser: @b regex class decls.
+
+	Copyright (c) 2001-2009 ArtLebedev Group (http://www.artlebedev.com)
+	Author: Alexandr Petrosian <paf@design.ru> (http://paf.design.ru)
+*/
+
+#ifndef PA_VREGEX_H
+#define PA_VREGEX_H
+
+static const char * const IDENT_VREGEX_H="$Date: 2009/04/22 04:34:14 $";
+
+// include
+
+#include "classes.h"
+#include "pa_common.h"
+#include "pa_vstateless_object.h"
+#include "pa_charset.h"
+#include "pcre.h"
+
+// defines
+
+#define VREGEX_TYPE "regex"
+
+enum Match_feature {
+	MF_GLOBAL_SEARCH = 0x01,
+	MF_NEED_PRE_POST_MATCH = 0x02,
+	MF_JUST_COUNT_MATCHES = 0x04
+};
+
+extern Methoded* regex_class;
+
+// VRegex
+class VRegex: public VStateless_object {
+
+public: // Value
+
+	override const char* type() const { return VREGEX_TYPE; }
+	override VStateless_class *get_class() { return regex_class; }
+
+	/// VRegex: count
+	override int as_int() { return get_info_size(); }
+
+	/// VRegex: count
+	override double as_double() { return as_int(); }
+
+	/// VRegex: true
+	override bool is_evaluated_expr() const { return true; }
+
+	/// VRegex: scalar
+	override Value& as_expr_result(bool/*return_string_as_is=false*/);
+
+	/// VRegex: true
+	virtual bool is_defined() const { return true; }
+
+	/// VRegex: true
+	override bool as_bool() const { return true; }
+
+public: // usage
+
+	VRegex():
+		fstudied(false) { }
+
+	VRegex(Charset& acharset, const String* aregex, const String* aoptions):
+		fstudied(false)
+	{
+		set(acharset, aregex, aoptions);
+		compile();
+	}
+
+	~VRegex(){
+		if(fextra)
+			pcre_free(fextra);
+		if(fcode)
+			pcre_free(fcode);
+	}
+
+	void set(Charset& acharset, const String* aregex, const String* aoptions);
+
+	void compile();
+
+	void study();
+
+	int exec(const char* string, size_t string_len, int* ovector, int ovector_size, int prestart=0);
+
+	// size_t info();
+
+	size_t full_info(int type);
+
+	size_t get_info_size();
+
+	size_t get_study_size();
+
+	size_t get_options();
+
+	bool is_pre_post_match_needed(){
+		return (foptions[1] & MF_NEED_PRE_POST_MATCH)!=0;
+	}
+
+	bool is_just_count(){
+		return (foptions[1] & MF_JUST_COUNT_MATCHES)!=0;
+	}
+
+	bool is_global_search(){
+		return (foptions[1] & MF_GLOBAL_SEARCH)!=0;
+	}
+
+private:
+	static void regex_options(const String* options, int* result);
+
+private:
+	Charset* fcharset;
+	const char* fpattern;
+	size_t fpattern_len;
+	int foptions[2];
+
+	pcre* fcode;
+	pcre_extra* fextra;
+	bool fstudied;
+};
+
+
+class VRegexCleaner {
+public:
+	VRegex *vregex;
+
+	VRegexCleaner(): vregex(0) { }
+
+	~VRegexCleaner(){
+		if(vregex) delete vregex;
+	}
+
+};
+
+
+#endif
