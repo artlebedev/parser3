bd1fd5938531af733ca449dbf0b89b297592df8d
Michael Petrushin <misha@design.ru>
2011-05-25 03:58:11 +0000

    - XmlException accepts more options

--- src/main/pa_xml_exception.C
+++ src/main/pa_xml_exception.C
@@ -9,7 +9,7 @@
 
 #ifdef XML
 
-static const char * const IDENT="$Date: 2005/12/16 10:15:12 $";
+static const char * const IDENT="$Date: 2011/05/25 03:58:11 $";
 
 #include "pa_xml_exception.h"
 #include "pa_globals.h"
@@ -17,13 +17,19 @@ static const char * const IDENT="$Date: 2005/12/16 10:15:12 $";
 
 // methods
 
-XmlException::XmlException(const String* aproblem_source) {
+XmlException::XmlException(const String* aproblem_source, const char* aproblem_comment, ...) {
 	ftype="xml";
 	fproblem_source=aproblem_source;
 
-	if(const char* xml_generic_errors=xmlGenericErrors()) {
+	if(aproblem_comment) {
+		fcomment=new(PointerFreeGC) char[MAX_STRING];
+		va_list args;
+		va_start(args, aproblem_comment);
+		vsnprintf(fcomment, MAX_STRING, aproblem_comment, args);
+		va_end(args);
+	} else if(const char* xml_generic_errors=xmlGenericErrors())
 		fcomment=pa_strdup(xml_generic_errors);
-	} else
+	else
 		fcomment="-UNKNOWN ERROR-";
 }
 #endif
