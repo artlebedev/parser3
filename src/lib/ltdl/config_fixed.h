a6e80866ca7d1591a693878f5216cc0d80c11320
Konstantin Morshnev <moko@artlebedev.ru>
2012-06-19 10:36:56 +0000

    compilation under Windows fixed

---
+++ src/lib/ltdl/config_fixed.h
@@ -0,0 +1,21 @@
+/* pa_config_fixed.h. */
+/* Configure file for autoconf-disabled platforms  */
+
+#define HAVE_CTYPE_H 1
+#define HAVE_ERRNO_H 1
+#define HAVE_MEMCPY 1
+#define HAVE_MEMMOVE 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STDIO_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRCHR 1
+#define HAVE_STRCMP 1
+#define HAVE_STRINGS_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRRCHR 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_TYPES_H 1
+
+#define error_t int
+#define LT_OBJDIR ".libs/"
+#define LT_MODULE_EXT ".dll"
