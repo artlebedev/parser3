93e9b46f5f2df2451eb9fda86086e244025947a8
Konstantin Morshnev <moko@artlebedev.ru>
2013-07-16 21:51:08 +0000

    headers actualized (inttypes.h added for uint8/32/64_t)

--- src/include/pa_config_fixed.h
+++ src/include/pa_config_fixed.h
@@ -8,32 +8,37 @@
 #ifndef PA_CONFIG_FIXED_H
 #define PA_CONFIG_FIXED_H
 
-#define IDENT_PA_CONFIG_FIXED_H "$Id: pa_config_fixed.h,v 1.79 2013/03/10 19:44:44 moko Exp $"
+#define IDENT_PA_CONFIG_FIXED_H "$Id: pa_config_fixed.h,v 1.80 2013/07/16 21:51:09 moko Exp $"
 
 #define inline  __inline
-#define HAVE_ASSERT_H
-#define HAVE_STDARG_H
-#define HAVE_PROCESS_H
-#define HAVE_FCNTL_H
-#define HAVE_SYS_STAT_H
-#define HAVE_IO_H
-#define HAVE_STDDEF_H
+
 #define HAVE_STDIO_H
-#define HAVE_ERRNO_H
+#define HAVE_SYS_TYPES_H
+#define HAVE_SYS_STAT_H
+#define HAVE_STDDEF_H
+#define HAVE_MEMORY_H
+#define HAVE_STRING_H
+
+#define HAVE_ASSERT_H
+#define HAVE_LIMITS_H
 #define HAVE_CTYPE_H
 #define HAVE_MATH_H
-#define HAVE_TIME_H
-#define HAVE_STRING_H
-#define HAVE_DIRECT_H
+#define HAVE_PROCESS_H
+#define HAVE_STDARG_H
 #define HAVE_SETJMP_H
-#define HAVE_MEMORY_H
-#define HAVE_LIMITS_H
+
+#define HAVE_ERRNO_H
+#define HAVE_FCNTL_H
+#define HAVE_IO_H
 #define HAVE_SYS_LOCKING_H
-#define HAVE_SYS_TYPES_H
-#define HAVE_WINSOCK_H
 #define HAVE__LOCKING
+
+#define HAVE_WINSOCK_H
+
+#define HAVE_TIME_H
 #define HAVE_TIMEZONE
 #define HAVE_DAYLIGHT
+
 #define USE_SMTP
 #define PA_LITTLE_ENDIAN
 #define PA_WITH_SJLJ_EXCEPTIONS
@@ -44,8 +49,16 @@
 typedef int ssize_t;
 #endif
 
+#ifndef uint8_t
+typedef unsigned __int8 uint8_t;
+#endif
+
 #ifndef uint32_t
-typedef unsigned int uint32_t;
+typedef unsigned __int32 uint32_t;
+#endif
+
+#ifndef uint64_t
+typedef unsigned __int64 uint64_t;
 #endif
 
 // disable any execs (file::exec, file::cgi, unix mail:send)
@@ -54,11 +67,9 @@ typedef unsigned int uint32_t;
 // disable stringstream usage
 //#define NO_STRINGSTREAM
 
-// has $mail:received
-//#define WITH_MAILRECEIVE
-
 //xml-abled parser
 #define XML
+
 //#define XML_STATIC
 #ifdef XML_STATIC
 #	define LIBXML_STATIC
--- src/include/pa_config_includes.h
+++ src/include/pa_config_includes.h
@@ -11,148 +11,153 @@
 #ifndef PA_CONFIG_INCLUDES_H
 #define PA_CONFIG_INCLUDES_H
 
-#if _MSC_VER
-#	pragma warning(disable:4291)   // disable warning 
-//	"no matching operator delete found; memory will not be freed if initialization throws an exception
-#endif
-
 #if HAVE_CONFIG_H
-#	include "pa_config_auto.h"
+# include "pa_config_auto.h"
 #else
-#	include "pa_config_fixed.h"
+# include "pa_config_fixed.h"
 #endif
 
-#ifdef __cplusplus
-#	undef	inline
-#endif
-
-#ifdef HAVE_ASSERT_H
-#	include <assert.h>
-#endif
-
-#ifdef HAVE_CTYPE_H
-#	include <ctype.h>
-#endif
-
-#ifdef HAVE_ERRNO_H
-#	include <errno.h>
-#endif
-
-#ifdef HAVE_FCNTL_H
-#	include <fcntl.h>
-#endif
-
-#ifdef HAVE_IO_H
-#	include <io.h>
-#endif
-
-#ifdef HAVE_PROCESS_H
-#	include <process.h>
-#endif
-
-#ifdef HAVE_STDARG_H
-#	include <stdarg.h>
-#endif
-
-#ifdef HAVE_STDDEF_H
-#	include <stddef.h>
-#endif
+// AC_INCLUDES_DEFAULT
 
 #ifdef HAVE_STDIO_H
-#	include <stdio.h>
-#endif
-
-#ifdef HAVE_SYS_STAT_H
-#	include <sys/stat.h>
-#endif
-
-#if defined(HAVE_UNISTD_H) && !defined(NO_UNISTD_H)
-#	include <unistd.h>
-#endif
-
-#ifdef HAVE_MATH_H
-#	include <math.h>
-#endif
-
-#include <stdlib.h>
-
-#ifdef HAVE_STRING_H
-#	include <string.h>
-#endif
-
-#ifdef HAVE_LIMITS_H
-#include <limits.h>
-#endif
-
-#ifdef HAVE_DIRECT_H
-#	include <direct.h>
-#endif
-
-#ifdef HAVE_SETJMP_H
-#	include <setjmp.h>
-#endif
-
-#ifdef HAVE_MEMORY_H
-#	include <memory.h>
-#endif
-
-#ifdef HAVE_NEW
-#include <new>
-#endif
-
-#ifdef HAVE_SYS_FILE_H
-#include <sys/file.h>
-#endif
-
-#ifdef HAVE_SYS_LOCKING_H
-#include <sys/locking.h>
+# include <stdio.h>
 #endif
 
 #ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h>
+# include <sys/types.h>
 #endif
 
-#ifdef HAVE_SYS_SELECT_H
-#include <sys/select.h>
+#ifdef HAVE_SYS_STAT_H
+# include <sys/stat.h>
+#endif
+
+#ifdef STDC_HEADERS
+# include <stdlib.h>
+# include <stddef.h>
+#else
+# ifdef HAVE_STDLIB_H
+#  include <stdlib.h>
+# endif
+#endif
+
+#ifdef HAVE_STRING_H
+# if !defined STDC_HEADERS && defined HAVE_MEMORY_H
+#  include <memory.h>
+# endif
+# include <string.h>
+#endif
+
+#ifdef HAVE_STRINGS_H
+# include <strings.h>
+#endif
+
+#ifdef HAVE_INTTYPES_H
+# include <inttypes.h>
+#endif
+
+#ifdef HAVE_STDINT_H
+# include <stdint.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+# include <unistd.h>
+#endif
+
+// other includes
+
+#ifdef HAVE_ASSERT_H
+# include <assert.h>
+#endif
+
+#ifdef HAVE_LIMITS_H
+# include <limits.h>
+#endif
+
+#ifdef HAVE_CTYPE_H
+# include <ctype.h>
+#endif
+
+#ifdef HAVE_MATH_H
+# include <math.h>
+#endif
+
+#ifdef HAVE_PROCESS_H
+# include <process.h>
+#endif
+
+#ifdef HAVE_STDARG_H
+# include <stdarg.h>
+#endif
+
+#ifdef HAVE_SETJMP_H
+# include <setjmp.h>
 #endif
 
 #ifdef HAVE_SIGNAL_H
-#include <signal.h>
+# include <signal.h>
 #endif
+
+#ifdef HAVE_ERRNO_H
+# include <errno.h>
+#endif
+
+#ifdef HAVE_FCNTL_H
+# include <fcntl.h>
+#endif
+
+#ifdef HAVE_IO_H
+# include <io.h>
+#endif
+
+#ifdef HAVE_SYS_FILE_H
+# include <sys/file.h>
+#endif
+
+#ifdef HAVE_SYS_LOCKING_H
+# include <sys/locking.h>
+#endif
+
+#ifdef HAVE_SYS_SELECT_H
+# include <sys/select.h>
+#endif
+
+#ifdef HAVE_SYS_RESOURCE_H
+# include <sys/resource.h>
+#endif
+
 
 #ifdef HAVE_SYS_SOCKET_H
-#	include <sys/socket.h>
+# include <sys/socket.h>
 #else
-#	if defined(HAVE_WINSOCK_H) && !defined(_MSC_VER) && !defined(CYGWIN)
-#		include <winsock.h>
-#	endif
+# if defined(HAVE_WINSOCK_H) && !defined(_MSC_VER) && !defined(CYGWIN)
+#  include <winsock.h>
+# endif
 #endif
-
-// must go anywhere after winsock [cygwin time.h checks for already inclusion of winsock.h]
-#if TIME_WITH_SYS_TIME
-#	include <sys/time.h>
-#	include <time.h>
-#else
-#	ifdef HAVE_SYS_TIME_H
-#		include <sys/time.h>
-#	else
-#		include <time.h>
-#	endif
-#endif
-
 
 #ifdef HAVE_NETINET_IN_H
-#include <netinet/in.h>
+# include <netinet/in.h>
 #endif
 
 #ifdef HAVE_ARPA_INET_H
-#include <arpa/inet.h>
+# include <arpa/inet.h>
 #endif
 
 #ifdef HAVE_NETDB_H
-#include <netdb.h>
+# include <netdb.h>
 #endif
 
+// must go anywhere after winsock [cygwin time.h checks for already inclusion of winsock.h]
+
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# ifdef HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 
 // math function replacements
 
@@ -186,36 +191,24 @@ inline size_t min(size_t a, size_t b){ return a<b?a:b; }
 #endif
 
 #if _MSC_VER
-// using this in calls to parent constructors
-#pragma warning(disable:4355)   
 
-//#	pragma warning(disable:4065)   // switch statement contains 'default' 
-								   // but no 'case' labels
-//#	pragma warning(disable:4800)   // (bool)(1&2)
+#pragma warning(disable:4355)  // using this in calls to parent constructors
 
-// assignment operator could not be generated
-//#pragma warning(disable:4512) 
+#pragma warning(disable:4291)  // no matching operator delete found; memory will not be freed if initialization throws an exception
 
-// identifier was truncated to '255' characters in the debug information
-#pragma warning(disable:4786)
+#pragma warning(disable:4786)  // identifier was truncated to '255' characters in the debug information
 
-// copy constructor could not be generated
-#pragma warning(disable:4511)
+#pragma warning(disable:4511)  // copy constructor could not be generated
 
-// assignment operator could not be generated
-#pragma warning(disable:4512)
+#pragma warning(disable:4512)  // assignment operator could not be generated
 
-// unreferenced local function has been removed
-#pragma warning(disable:4505)
+#pragma warning(disable:4505)  // unreferenced local function has been removed
 
-// conditional expression is constant; while(true) OK
-#pragma warning(disable:4127)
+#pragma warning(disable:4127)  // conditional expression is constant; while(true) OK
 
-// assignment within conditional expression if(type var=somethig) OK
-#pragma warning(disable:4706)
+#pragma warning(disable:4706)  // assignment within conditional expression if(type var=somethig) OK
 
-// memcpy deprecation
-#pragma warning(disable:4996)
+#pragma warning(disable:4996)  // memcpy deprecation
 
 #endif
 
