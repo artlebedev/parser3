c08f267bba786606cdbcf8e623a567c6734dea1a
Konstantin Morshnev <moko@artlebedev.ru>
2013-10-15 22:31:14 +0000

    3.4.3b -> 3.4.3rc

--- src/include/pa_version.h
+++ src/include/pa_version.h
@@ -1,4 +1,4 @@
 /* specified manually on Windows [automaticaly set on Unix] */
 #ifndef PARSER_VERSION
-#define PARSER_VERSION "3.4.3b (compiled on i386-pc-win32)"
+#define PARSER_VERSION "3.4.3rc (compiled on i386-pc-win32)"
 #endif
