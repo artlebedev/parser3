c8936d4bf73ecfae08ad3123ea538587f607f07f
Konstantin Morshnev <moko@artlebedev.ru>
2015-09-28 21:38:24 +0000

    warning war

--- src/include/pa_config_includes.h
+++ src/include/pa_config_includes.h
@@ -192,6 +192,9 @@ inline size_t min(size_t a, size_t b){ return a<b?a:b; }
 #define THREAD_LOCAL // __thread // multithreading support required only for apache2 module
 #endif
 
+#pragma clang diagnostic ignored "-Wparentheses"       // if(a=b)
+#pragma clang diagnostic ignored "-Winline-new-delete" // replacement operator cannot be declared 'inline'
+
 #if _MSC_VER
 
 #pragma warning(disable:4355)  // using this in calls to parent constructors
