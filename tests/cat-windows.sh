99ffeed4801e85f96537c5f3626c56abf313c55a
Konstantin Morshnev <moko@artlebedev.ru>
2017-02-13 17:17:37 +0000

    cat-windows added (cygwin required)

---
+++ tests/cat-windows.sh
@@ -0,0 +1,31 @@
+#!c:\cygwin\bin\sh.exe
+
+if [ $1 = "halt" ]
+then
+	exit $2
+fi
+
+if [ $1 = "stdin" ]
+then
+	c:/cygwin/bin/cat.exe
+	exit 0
+fi
+
+if [ $1 = "cgi" ]
+then
+	echo "content-type: $2
+"
+fi
+
+if [ -n "$5" ]
+then
+	echo "$5"
+fi
+
+c:/cygwin/bin/cat.exe -s $3
+
+if [ -n "$4" ]
+then
+	exit $4
+fi
+
