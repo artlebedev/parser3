9d8802f2deb47209c919e18c5dc41e50151d617e
Konstantin Morshnev <moko@artlebedev.ru>
2017-05-25 15:37:45 +0000

    no more "this property has no setter method" when setting $o.prop without setter (implements feature #1157)

--- src/types/pa_vclass.C
+++ src/types/pa_vclass.C
@@ -8,7 +8,7 @@
 #include "pa_vclass.h"
 #include "pa_vobject.h"
 
-volatile const char * IDENT_PA_VCLASS_C="$Id: pa_vclass.C,v 1.56 2017/02/07 22:00:46 moko Exp $" IDENT_PA_VCLASS_H;
+volatile const char * IDENT_PA_VCLASS_C="$Id: pa_vclass.C,v 1.57 2017/05/25 15:37:45 moko Exp $" IDENT_PA_VCLASS_H;
 
 #ifdef OBJECT_PROTOTYPE
 	bool VClass::prototype = true;
@@ -150,17 +150,15 @@ const VJunction* VClass::put_element_replace_only(Value& aself, const String& an
 	if(Property* prop=ffields.get(aname)) {
 		if (prop->setter)
 			return new VJunction(aself, prop->setter);
-
-		if(prop->getter){
-			if(VJunction *result=get_default_setter(aself, aname))
-				return result;
-			throw Exception(PARSER_RUNTIME,	0, "this property has no setter method (@SET_%s[value])", aname.cstr());
-		}
-		
 #ifdef OBJECT_PROTOTYPE
 		if(!prototype)
 #endif
 		{
+			if(prop->getter){
+				if(VJunction *result=get_default_setter(aself, aname))
+					return result;
+				throw Exception(PARSER_RUNTIME,	0, "this property has no setter method (@SET_%s[value])", aname.cstr());
+			}
 			// just field, value can be 0 and unlike usual we don't remove it
 			prop->value=avalue;
 			return PUT_ELEMENT_REPLACED_ELEMENT;
