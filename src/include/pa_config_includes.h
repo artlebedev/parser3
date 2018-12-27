b8a9411658cee54194bca6bf9e93de088d8699a1
Konstantin Morshnev <moko@artlebedev.ru>
2018-12-27 10:41:06 +0000

    warning war: -Wdeprecated-register added for C++ 17

--- src/include/pa_config_includes.h
+++ src/include/pa_config_includes.h
@@ -222,9 +222,10 @@ inline size_t min(size_t a, size_t b){ return a<b?a:b; }
 #endif
 
 #ifdef __clang__
-#pragma clang diagnostic ignored "-Wparentheses"       // if(a=b)
-#pragma clang diagnostic ignored "-Wpointer-sign"      // CORD (unsigned char *) to char * in libcord
-#pragma clang diagnostic ignored "-Winline-new-delete" // test for regular new/delete usage in memory.h
+#pragma clang diagnostic ignored "-Wparentheses"         // if(a=b)
+#pragma clang diagnostic ignored "-Wpointer-sign"        // CORD (unsigned char *) to char * in libcord
+#pragma clang diagnostic ignored "-Winline-new-delete"   // test for regular new/delete usage in memory.h
+#pragma clang diagnostic ignored "-Wdeprecated-register" // 'register' specifier is deprecated and incompatible with C++17
 #endif
 
 #ifdef _MSC_VER
