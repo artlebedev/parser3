e9a5e6e8a57c2853fd3f06c17e176165a3869307
Konstantin Morshnev <moko@artlebedev.ru>
2020-12-24 07:18:42 +0000

    looks like xsltFreeStylesheet leads to memory double-free. If so, it's for sure libxml bug, but we have GC anyway, so we just remove this call and see...
    * thread #1, name = 'parser.parser3.cgi', stop reason = signal SIGABRT
      * frame #0: 0x0000000801ab50fa libc.so.7`__sys_thr_kill + 10
        frame #2: 0x0000000801ab5039 libc.so.7`abort at abort.c:65:8
        frame #3: 0x00000000004da663 parser.parser3.cgi`GC_freehblk(hbp=<unavailable>) at allchblk.c:878:9
        frame #4: 0x00000000004cb8c4 parser.parser3.cgi`GC_free(p=0x0000000808b34000) at malloc.c:617:9
        frame #5: 0x0000000000577f0c parser.parser3.cgi`xmlHashFree(table=0x0000000806991a80, f=<unavailable>) at hash.c:356:2
        frame #6: 0x000000000051a7c2 parser.parser3.cgi`xsltFreeTemplateHashes + 50
        frame #7: 0x000000000050cfac parser.parser3.cgi`xsltFreeStylesheet + 124
        frame #8: 0x000000000050d35c parser.parser3.cgi`xsltFreeStylesheetList + 44
        frame #9: 0x000000000050d1fa parser.parser3.cgi`xsltFreeStylesheet + 714
        frame #10: 0x00000000004436e5 parser.parser3.cgi`Stylesheet_manager::maybe_expire_cache() [inlined] Stylesheet_connection::disconnect(this=0x0000000804676c80) at pa_stylesheet_connection.h:62:3

--- src/include/pa_stylesheet_connection.h
+++ src/include/pa_stylesheet_connection.h
@@ -8,7 +8,7 @@
 #ifndef PA_STYLESHEET_CONNECTION_H
 #define PA_STYLESHEET_CONNECTION_H
 
-#define IDENT_PA_STYLESHEET_CONNECTION_H "$Id: pa_stylesheet_connection.h,v 1.47 2020/12/15 17:10:32 moko Exp $"
+#define IDENT_PA_STYLESHEET_CONNECTION_H "$Id: pa_stylesheet_connection.h,v 1.48 2020/12/24 07:18:42 moko Exp $"
 
 #include "libxslt/xslt.h"
 #include "libxslt/xsltInternals.h"
@@ -21,7 +21,7 @@
 
 // defines
 
-/**	Connection with stylesheet: 
+/**	Connection with stylesheet:
 	remembers time and can figure out that it needs recompilation
 */
 class Stylesheet_connection: public PA_Object {
@@ -44,13 +44,13 @@ public:
 		dependencies(0),
 		time_used(0),
 		prev_disk_time(0),
-		used(0)  
+		used(0)
 	{}
-	
+
 	String::Body file_spec() { return ffile_spec; }
 
 	bool uncachable() {
-		return !dependencies/*means they were external*/;
+		return !dependencies /*means they were external*/;
 	}
 
 	bool expired(time_t older_dies) {
@@ -58,17 +58,17 @@ public:
 	}
 	time_t get_time_used() { return time_used; }
 
-	void disconnect() { 
-		xsltFreeStylesheet(fstylesheet);  fstylesheet=0; 
+	void disconnect() {
+		fstylesheet=0;
 	}
 
 	bool connected() { return fstylesheet!=0; }
 
-	xsltStylesheet *stylesheet() { 
+	xsltStylesheet *stylesheet() {
 		time_t new_disk_time=0;
 		if(uncachable() || (new_disk_time=get_new_disk_time()))
 			load(new_disk_time);
-		return fstylesheet; 
+		return fstylesheet;
 	}
 
 private:
@@ -89,10 +89,12 @@ private:
 private: // connection usage methods
 
 	int used;
+
 	void use() {
 		time_used=time(0); // they started to use at this time
 		used++;
 	}
+
 	void unuse() {
 		used--;
 		if(!used)
