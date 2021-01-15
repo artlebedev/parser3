7b05198668eb67fccfcea9749346b978a357861c
Konstantin Morshnev <moko@artlebedev.ru>
2021-01-15 20:26:25 +0000

    3.4.6b -> 3.4.6rc

--- src/include/pa_version.h
+++ src/include/pa_version.h
@@ -1,8 +1,8 @@
 /* specified manually on Windows [automaticaly set on Unix] */
 #ifndef PARSER_VERSION
 #ifdef _WIN64
-#define PARSER_VERSION "3.4.6b (compiled on amd64-pc-win64)"
+#define PARSER_VERSION "3.4.6rc (compiled on amd64-pc-win64)"
 #else
-#define PARSER_VERSION "3.4.6b (compiled on i386-pc-win32)"
+#define PARSER_VERSION "3.4.6rc (compiled on i386-pc-win32)"
 #endif
 #endif
